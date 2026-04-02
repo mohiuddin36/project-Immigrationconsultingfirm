12bb50e10b1e930f13cce26af9f1073ee974d373
mohiuddin36 <islammohiuddin270@gmail.com>
2026-04-02 15:52:43 +0600

    first push s

---
+++ IMMIGRATIONCONSULTINGFIRM_Project_cse213/src/main/java/com/example/immigrationconsultingfirm_project_cse213/AppointmentCoordinator/ViewAppointmentRequests.java
@@ -0,0 +1,7 @@
+package com.example.immigrationconsultingfirm_project_cse213.AppointmentCoordinator;
+
+public class ViewAppointmentRequests
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
