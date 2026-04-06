7f84a78355c97f2b2bea5e7b90f93ccc3d9ff4b3
mohiuddin36 <islammohiuddin270@gmail.com>
2026-04-07 05:33:45 +0600

    add my changes

---
+++ IMMIGRATIONCONSULTINGFIRM_Project_cse213/src/main/java/com/example/immigrationconsultingfirm_project_cse213/AppointmentCoordinator/CancelAppoinment.java
@@ -0,0 +1,7 @@
+package com.example.immigrationconsultingfirm_project_cse213.AppointmentCoordinator;
+
+public class CancelAppoinment
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
---
+++ IMMIGRATIONCONSULTINGFIRM_Project_cse213/src/main/java/com/example/immigrationconsultingfirm_project_cse213/AppointmentCoordinator/ScheduleNewAppointment.java
@@ -0,0 +1,7 @@
+package com.example.immigrationconsultingfirm_project_cse213.AppointmentCoordinator;
+
+public class ScheduleNewAppointment
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
