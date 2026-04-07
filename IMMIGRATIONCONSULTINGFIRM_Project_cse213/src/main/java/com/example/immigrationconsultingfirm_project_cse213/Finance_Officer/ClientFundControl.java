513407823fa30cfbcdf31cb29c52815d24a34289
Rokeya Khatun <2430934@iub.edu.com>
2026-04-08 02:16:33 +0600

    generat controller

--- IMMIGRATIONCONSULTINGFIRM_Project_cse213/src/main/java/com/example/immigrationconsultingfirm_project_cse213/Finance_Officer/ClientFundControl.java
+++ IMMIGRATIONCONSULTINGFIRM_Project_cse213/src/main/java/com/example/immigrationconsultingfirm_project_cse213/Finance_Officer/ClientFundControl.java
@@ -7,4 +7,25 @@ public class ClientFundControl implements Serializable {
     protected String name;
     protected int id;
     protected LocalDate date;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ClientFundControl(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "ClientFundControl{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", date=" + date +
+                '}';
+    }
 }
