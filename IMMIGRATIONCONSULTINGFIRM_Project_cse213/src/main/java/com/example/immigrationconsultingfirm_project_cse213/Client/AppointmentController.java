0d5429c34ad4fe24106d22937710212da3efe231
Syeda Busrat Jarin <syedabusratjarin@gmail.com>
2026-04-09 22:50:24 +0600

    add

---
+++ IMMIGRATIONCONSULTINGFIRM_Project_cse213/src/main/java/com/example/immigrationconsultingfirm_project_cse213/Client/AppointmentController.java
@@ -0,0 +1,7 @@
+package com.example.immigrationconsultingfirm_project_cse213.Client;
+
+public class AppointmentController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
---
+++ IMMIGRATIONCONSULTINGFIRM_Project_cse213/src/main/java/com/example/immigrationconsultingfirm_project_cse213/Staff/AppointmentManageController.java
@@ -0,0 +1,7 @@
+package com.example.immigrationconsultingfirm_project_cse213.Staff;
+
+public class AppointmentManageController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
