fadd49824d8d8426bb921911663166ab6b0f3c1f
Rokeya Khatun <2430934@iub.edu.com>
2026-04-10 10:27:33 +0600

    Back Button code of Finance Officer

--- IMMIGRATIONCONSULTINGFIRM_Project_cse213/src/main/java/com/example/immigrationconsultingfirm_project_cse213/Finance_Officer/Controller/BudgetMonitor.java
+++ IMMIGRATIONCONSULTINGFIRM_Project_cse213/src/main/java/com/example/immigrationconsultingfirm_project_cse213/Finance_Officer/Controller/BudgetMonitor.java
@@ -1,8 +1,15 @@
 package com.example.immigrationconsultingfirm_project_cse213.Finance_Officer.Controller;
 
+import com.example.immigrationconsultingfirm_project_cse213.HelloApplication;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class BudgetMonitor
 {
@@ -18,7 +25,13 @@ public class BudgetMonitor
     }
 
     @javafx.fxml.FXML
-    public void backButton(ActionEvent actionEvent) {
+    public void backButton(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("FinanceOfficer/financeOfficerDashboard.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setTitle("Steady Cash Flow");
+        stage.setScene(scene);
+        stage.show();
     }
 
     @javafx.fxml.FXML
--- IMMIGRATIONCONSULTINGFIRM_Project_cse213/src/main/java/com/example/immigrationconsultingfirm_project_cse213/Finance_Officer/Controller/MonthlyFinancereports.java
+++ IMMIGRATIONCONSULTINGFIRM_Project_cse213/src/main/java/com/example/immigrationconsultingfirm_project_cse213/Finance_Officer/Controller/MonthlyFinancereports.java
@@ -1,6 +1,13 @@
 package com.example.immigrationconsultingfirm_project_cse213.Finance_Officer.Controller;
 
+import com.example.immigrationconsultingfirm_project_cse213.HelloApplication;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MonthlyFinancereports
 {
@@ -17,7 +24,13 @@ public class MonthlyFinancereports
     }
 
     @javafx.fxml.FXML
-    public void backButton(ActionEvent actionEvent) {
+    public void backButton(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("FinanceOfficer/financeOfficerDashboard.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setTitle("Monthly finance reports");
+        stage.setScene(scene);
+        stage.show();
     }
 
     @javafx.fxml.FXML
