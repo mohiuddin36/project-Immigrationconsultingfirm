b9a806b53316d952619dad07e21050031d53d01b
Rokeya Khatun <2430934@iub.edu.com>
2026-04-10 15:29:56 +0600

    visa rule Initialize

--- IMMIGRATIONCONSULTINGFIRM_Project_cse213/src/main/java/com/example/immigrationconsultingfirm_project_cse213/Finance_Officer/Controller/PaymentBreakdown.java
+++ IMMIGRATIONCONSULTINGFIRM_Project_cse213/src/main/java/com/example/immigrationconsultingfirm_project_cse213/Finance_Officer/Controller/PaymentBreakdown.java
@@ -9,6 +9,7 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -20,18 +21,23 @@ public class PaymentBreakdown
     @javafx.fxml.FXML
     private TextField nameTaxtField;
     @javafx.fxml.FXML
-    private TableColumn<PaymentBreakdown,String> applicationStreamTableView;
-    @javafx.fxml.FXML
     private ComboBox<String> applicationStreamComboBox;
-    @javafx.fxml.FXML
-    private TableColumn<PaymentBreakdown,String> clientIdTableView;
     @javafx.fxml.FXML
     private TableView<String> tableView;
     @javafx.fxml.FXML
-    private TableColumn<PaymentBreakdown,String> clientNameTableView;
+    private TableColumn<PaymentBreakdown,String> applicationStreamTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn<PaymentBreakdown,Integer> clientIdTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn<PaymentBreakdown,String> clientNameTableColumn;
 
     @javafx.fxml.FXML
     public void initialize() {
+        applicationStreamComboBox.getItems().addAll("Paid","Unpaid");
+        clientIdTableColumn.setCellValueFactory(new PropertyValueFactory<>("clintId"));
+        clientNameTableColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        applicationStreamComboBox.setCellFactory(new PropertyValueFactory<>("applicationStream"));
+
     }
 
     @javafx.fxml.FXML
