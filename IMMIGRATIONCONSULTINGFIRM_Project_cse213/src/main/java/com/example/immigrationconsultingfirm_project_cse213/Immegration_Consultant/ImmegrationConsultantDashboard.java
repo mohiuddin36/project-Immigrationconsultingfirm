7e23d1d01bf9b391c5f507a4cd78d90d28e015a4
Rokeya Khatun <2430934@iub.edu.com>
2026-04-10 16:28:58 +0600

    Immigration Consultant Dashboard button

--- IMMIGRATIONCONSULTINGFIRM_Project_cse213/src/main/java/com/example/immigrationconsultingfirm_project_cse213/Immegration_Consultant/ImmegrationConsultantDashboard.java
+++ IMMIGRATIONCONSULTINGFIRM_Project_cse213/src/main/java/com/example/immigrationconsultingfirm_project_cse213/Immegration_Consultant/ImmegrationConsultantDashboard.java
@@ -1,6 +1,13 @@
 package com.example.immigrationconsultingfirm_project_cse213.Immegration_Consultant;
 
+import com.example.immigrationconsultingfirm_project_cse213.HelloApplication;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ImmegrationConsultantDashboard
 {
@@ -10,10 +17,10 @@ public class ImmegrationConsultantDashboard
 
     @javafx.fxml.FXML
     public void applicationsubmission(ActionEvent actionEvent)throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Immegration/applicationSubmission-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("FinanceOfficer/paymentBreakdown.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        stage.setTitle("Application submission");
+        stage.setTitle("Payment Breakdown");
         stage.setScene(scene);
         stage.show();
     }
