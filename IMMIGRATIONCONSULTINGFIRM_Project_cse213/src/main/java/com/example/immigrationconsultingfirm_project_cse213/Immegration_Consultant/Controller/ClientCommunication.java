06c3ba8f2b1a5da64b20812918919d7cfa26bb99
Rokeya Khatun <2430934@iub.edu.com>
2026-04-10 23:18:11 +0600

    Client Communication

--- IMMIGRATIONCONSULTINGFIRM_Project_cse213/src/main/java/com/example/immigrationconsultingfirm_project_cse213/Immegration_Consultant/Controller/ClientCommunication.java
+++ IMMIGRATIONCONSULTINGFIRM_Project_cse213/src/main/java/com/example/immigrationconsultingfirm_project_cse213/Immegration_Consultant/Controller/ClientCommunication.java
@@ -1,5 +1,6 @@
 package com.example.immigrationconsultingfirm_project_cse213.Immegration_Consultant.Controller;
 
+import com.example.immigrationconsultingfirm_project_cse213.Finance_Officer.Model.AppendableObjectOutputStream;
 import com.example.immigrationconsultingfirm_project_cse213.HelloApplication;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
@@ -9,7 +10,10 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 
 public class ClientCommunication
 {
@@ -27,17 +31,36 @@ public class ClientCommunication
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Immegration Consultant/immegrationConsultantDashboard.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        stage.setTitle("Client Communication");
+        stage.setTitle("Immigration Consultant Dashboard");
         stage.setScene(scene);
         stage.show();
     }
 
     @javafx.fxml.FXML
     public void saveButton(ActionEvent actionEvent) {
+        ClientCommunication cc = new ClientCommunication(
+                //nameTextField.getText();
+                //Integer.parseInt(idTextField.getText());
+        );
+        try {
+            File file = new File("ClientCommunication.bin");
+            FileOutputStream fos;
+            ObjectOutputStream oos;
 
+            if (file.exists()) {
+                fos = new FileOutputStream(file, true);
+                oos = new AppendableObjectOutputStream(fos);
+            } else {
+                fos = new FileOutputStream(file);
+                oos = new ObjectOutputStream(fos);
+            }
+            oos.writeObject(cc);
+            oos.close();
+            informationAlert("Added successfully!");
 
-
-
+        } catch (Exception e) {
+            errorAlert("Error saving data to file!");
+        }
 
     }
     public void errorAlert(String s){
@@ -49,4 +72,11 @@ public class ClientCommunication
         Alert a = new Alert(Alert.AlertType.INFORMATION);
         a.setContentText(s);
         a.showAndWait();
+    }
+
 }
+
+
+
+
+
