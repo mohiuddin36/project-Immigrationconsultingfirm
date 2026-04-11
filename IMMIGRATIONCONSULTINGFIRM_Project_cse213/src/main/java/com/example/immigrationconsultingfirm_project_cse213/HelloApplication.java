b4c6de3d9423cdda67512185062509af666f9cda
Rokeya Khatun <2430934@iub.edu.com>
2026-04-11 19:41:34 +0600

    All uploded

--- IMMIGRATIONCONSULTINGFIRM_Project_cse213/src/main/java/com/example/immigrationconsultingfirm_project_cse213/HelloApplication.java
+++ IMMIGRATIONCONSULTINGFIRM_Project_cse213/src/main/java/com/example/immigrationconsultingfirm_project_cse213/HelloApplication.java
@@ -11,16 +11,14 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(
-<<<<<<< HEAD
+
                 HelloApplication.class.getResource("FinanceOfficer/financeOfficerDashboard.fxml")
-=======
-<<<<<<< HEAD
-                HelloApplication.class.getResource(
-                        "/com/example/immigrationconsultingfirmprojectcse213/Documentation/AddClient.fxml")
-=======
-                HelloApplication.class.getResource("com.example.immigrationconsultingfirm_project_cse213.HelloController")
->>>>>>> af7354aa15cfda3d1705edbbce1c3292d77e5810
->>>>>>> fe4ee4991276fa18193f15c4dd9f6570f122f25f
+
+//                HelloApplication.class.getResource(
+//                        "/com/example/immigrationconsultingfirmprojectcse213/Documentation/AddClient.fxml")
+//
+//                HelloApplication.class.getResource("com.example.immigrationconsultingfirm_project_cse213.HelloController")
+
         );
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
