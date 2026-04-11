063bfb0a4fa729aaa58a2da2951cad22cd2d5e55
Syeda Busrat Jarin <syedabusratjarin@gmail.com>
2026-04-12 00:13:53 +0600

    clean project structure - removed extra folders

--- SyedaBusratJarin-2430812/src/main/java/com/example/javafxproject/MessageStorage.java
+++
@@ -1,8 +0,0 @@
-package com.example.javafxproject;
-
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-
-public class MessageStorage {
-    public static ObservableList<Message> messages = FXCollections.observableArrayList();
-}
